ab5515c6c74de6ee0418bdd817e561a0c50c296f
Emily Weil <34519397+emilyweil@users.noreply.github.com>
2026-06-10 14:57:54 -0400

    overlay adjustment

--- api/earth.js
+++ api/earth.js
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 
 export default function handler(req, res) {
-  const file = path.join(process.cwd(), "pinksky.png");
+  const file = path.join(process.cwd(), "rainbowsky.png");
   const img = fs.readFileSync(file);
   res.setHeader("Content-Type", "image/png");
   res.status(200).send(img);
