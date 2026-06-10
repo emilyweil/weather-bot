7199324f4af6ca63fd2c535bf0e821708c1d2a6d
Emily Weil <34519397+emilyweil@users.noreply.github.com>
2026-06-10 14:35:25 -0400

    Swap to red sky background image

--- api/earth.js
+++ api/earth.js
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 
 export default function handler(req, res) {
-  const file = path.join(process.cwd(), "earth.png");
+  const file = path.join(process.cwd(), "sky.png");
   const img = fs.readFileSync(file);
   res.setHeader("Content-Type", "image/png");
   res.status(200).send(img);
