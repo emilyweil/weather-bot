b5569d5310660ab19203aea24b990573322faff0
Emily Weil <34519397+emilyweil@users.noreply.github.com>
2026-09-24 10:44:50 -0400

    Add Red Sky favicon

--- api/favicon.js
+++ api/favicon.js
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 
 export default function handler(req, res) {
-  const file = path.join(process.cwd(), "RedSkyFlavicon1.png");
+  const file = path.join(process.cwd(), "RedSkyFavicon1.png");
   const img = fs.readFileSync(file);
   res.setHeader("Content-Type", "image/png");
   res.setHeader("Cache-Control", "public, max-age=86400");
